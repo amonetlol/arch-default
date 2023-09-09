c4b9cdb65a05129dc3ad1aec4d101bb571d40f6d
Pio <aaa@aaa.com>
2023-09-09 15:05:00 -0300

    first commit

--- default.sh
+++ default.sh
@@ -10,39 +10,49 @@ dir=$(pwd)
 source $dir/library.sh
 
 # Set color
-yellow="\e[33m]"
+yellow="\e[1;33m]"
 reset_cor="\e[0m]"
 
-
+echo -e "${yellow}--------------------------------------------${reset_cor}"
+echo $'\n'
 echo -e "${yellow}Atualizando chaves do Archlinux. ${reset_cor}"
+echo $'\n'
+
 sudo pacman -Sy archlinux-keyring --noconfirm
 
 # ---- Yay ---- #
 if sudo pacman -Qs git > /dev/null ; then
     echo -e "${yellow}Git está instalado. ${reset_cor}"
+    echo $'\n'
 else
-    echo "Instalando o git!"
+    echo "${yellow}Instalando o git!${reset_cor}"
+    echo $'\n'
     sudo pacman -Sy git --noconfirm
     clear
     echo -e "${yellow}Git instalado com sucesso! ${reset_cor}"
+    echo $'\n'
 fi
 
 # ---- Yay ---- #
 if sudo pacman -Qs yay > /dev/null ; then
     echo -e "${yellow}Yay está instalado. ${reset_cor}"
+    echo $'\n'
 else
     echo -e "${yellow}Instalando o yay! ${reset_cor}"
-    sudo pacman -Sy go
+    echo $'\n'
+    sudo pacman -Sy go --noconfirm
     #git clone https://aur.archlinux.org/yay-git.git
     git clone https://aur.archlinux.org/yay-bin.git
     cd $dir/yay-bin
     makepkg -si
     clear
     echo -e "${yellow}Yay instalado com sucesso! ${reset_cor}"
+    echo $'\n'
 fi
 
 # ---- Instalado pacotes padrõese ---- #
 echo -e "${yellow}Instalando os pacotes. ${reset_cor}"
+echo $'\n'
 
 packagesPacman=(
   "neovim"
@@ -84,12 +94,14 @@ while true; do
     case $sn in
         [Ss]* )
             echo -e "{yellow}Instalando os drivers do VMware. ${reset_cor}"
+            echo $'\n'
             sudo pacman -Sy open-vm-tools xf86-input-vmmouse xf86-video-vmware gtkmm3 fuse2 --noconfirm
             echo -e "${yellow}Iniciando o serviço vmtoolsd. ${reset_cor}"
+            echo $'\n'
             sudo systemctl enable vmtoolsd.service
         break;;
         [Nn]* ) 
-            exit;
+            #exit;
         break;;
         * ) echo "Sim ou não.";;
     esac
@@ -101,13 +113,15 @@ while true; do
     case $sn in
         [Ss]* )
             echo -e "${yellow}Instalando o SDDM. ${reset_cor}"
+            echo $'\n'
             sudo pacman -Sy glibc --noconfirm
             yay -S sddm-git --noconfirm
             echo -e "${yellow}Iniciando o serviço SDDM. ${reset_cor}"
+            echo $'\n'
             sudo systemctl enable sddm.service
         break;;
         [Nn]* ) 
-            exit;
+            #exit;
         break;;
         * ) echo "Sim ou não.";;
     esac
@@ -119,10 +133,11 @@ while true; do
     case $sn in
         [Ss]* )
             echo -e "${yellow}Instalando o Nemo. ${reset_cor}"
-            yay -S nemo nemo-emblems nemo-fileroller cinnamon-translations unrar unzip
+            echo $'\n'
+            yay -S nemo nemo-emblems nemo-fileroller cinnamon-translations unrar unzip --noconfirm
         break;;
         [Nn]* ) 
-            exit;
+            #exit;
         break;;
         * ) echo "Sim ou não.";;
     esac
@@ -134,10 +149,11 @@ while true; do
     case $sn in
         [Ss]* )
             echo -e "${yellow}Instalando o Qtile. ${reset_cor}"
+            echo $'\n'
             sudo pacman -S - < $dir/qtile.txt
         break;;
         [Nn]* ) 
-            exit;
+            #exit;
         break;;
         * ) echo "Sim ou não.";;
     esac
@@ -149,10 +165,11 @@ while true; do
     case $sn in
         [Ss]* )
             echo -e "${yellow}Instalando o Hyprland. ${reset_cor}"
+            echo $'\n'
             sudo pacman -S - < $dir/hyprland.txt
         break;;
         [Nn]* ) 
-            exit;
+            #exit;
         break;;
         * ) echo "Sim ou não.";;
     esac
