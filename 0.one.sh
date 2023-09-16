e306c87db13304000d35614d22f4553497d5bbd5
Pio <aaa@aaa.com>
2023-09-16 12:26:57 -0300

    first commit

---
+++ 0.one.sh
@@ -0,0 +1,339 @@
+#!/bin/sh
+
+dir=$(pwd) 
+CONFIGS_DIR=$dir/configs/
+CONFIG_FILE=$dir/configs/setup.conf
+if [ ! -f $CONFIG_FILE ]; then # check if file exists
+    touch -f $CONFIG_FILE # create file if not exists
+fi
+
+# Nome do arquivo de log
+log_file="0.log"
+
+# Redireciona a saída padrão e a saída de erro padrão para o arquivo de log
+exec > >(tee -a "$log_file") 2>&1
+
+root_check() {
+    if [[ "$(id -u)" != "0" ]]; then
+        echo -ne "ERROR! This script must be run under the 'root' user!\n"
+        exit 0
+    fi
+}
+
+docker_check() {
+    if awk -F/ '$2 == "docker"' /proc/self/cgroup | read -r; then
+        echo -ne "ERROR! Docker container is not supported (at the moment)\n"
+        exit 0
+    elif [[ -f /.dockerenv ]]; then
+        echo -ne "ERROR! Docker container is not supported (at the moment)\n"
+        exit 0
+    fi
+}
+
+arch_check() {
+    if [[ ! -e /etc/arch-release ]]; then
+        echo -ne "ERROR! This script must be run in Arch Linux!\n"
+        exit 0
+    fi
+}
+
+pacman_check() {
+    if [[ -f /var/lib/pacman/db.lck ]]; then
+        echo "ERROR! Pacman is blocked."
+        echo -ne "If not running remove /var/lib/pacman/db.lck.\n"
+        exit 0
+    fi
+}
+
+background_checks() {
+    root_check
+    arch_check
+    pacman_check
+    docker_check
+}
+
+set_option() {
+    if grep -Eq "^${1}.*" $CONFIG_FILE; then # check if option exists
+        sed -i -e "/^${1}.*/d" $CONFIG_FILE # delete option if exists
+    fi
+    echo "${1}=${2}" >>$CONFIG_FILE # add option
+}
+
+# Renders a text based list of options that can be selected by the
+# user using up, down and enter keys and returns the chosen option.
+#
+#   Arguments   : list of options, maximum of 256
+#                 "opt1" "opt2" ...
+#   Return value: selected index (0 for opt1, 1 for opt2 ...)
+select_option() {
+
+    # little helpers for terminal print control and key input
+    ESC=$( printf "\033")
+    cursor_blink_on()  { printf "$ESC[?25h"; }
+    cursor_blink_off() { printf "$ESC[?25l"; }
+    cursor_to()        { printf "$ESC[$1;${2:-1}H"; }
+    print_option()     { printf "$2   $1 "; }
+    print_selected()   { printf "$2  $ESC[7m $1 $ESC[27m"; }
+    get_cursor_row()   { IFS=';' read -sdR -p $'\E[6n' ROW COL; echo ${ROW#*[}; }
+    get_cursor_col()   { IFS=';' read -sdR -p $'\E[6n' ROW COL; echo ${COL#*[}; }
+    key_input()         {
+                        local key
+                        IFS= read -rsn1 key 2>/dev/null >&2
+                        if [[ $key = ""      ]]; then echo enter; fi;
+                        if [[ $key = $'\x20' ]]; then echo space; fi;
+                        if [[ $key = "k" ]]; then echo up; fi;
+                        if [[ $key = "j" ]]; then echo down; fi;
+                        if [[ $key = "h" ]]; then echo left; fi;
+                        if [[ $key = "l" ]]; then echo right; fi;
+                        if [[ $key = "a" ]]; then echo all; fi;
+                        if [[ $key = "n" ]]; then echo none; fi;
+                        if [[ $key = $'\x1b' ]]; then
+                            read -rsn2 key
+                            if [[ $key = [A || $key = k ]]; then echo up;    fi;
+                            if [[ $key = [B || $key = j ]]; then echo down;  fi;
+                            if [[ $key = [C || $key = l ]]; then echo right;  fi;
+                            if [[ $key = [D || $key = h ]]; then echo left;  fi;
+                        fi 
+    }
+    print_options_multicol() {
+        # print options by overwriting the last lines
+        local curr_col=$1
+        local curr_row=$2
+        local curr_idx=0
+
+        local idx=0
+        local row=0
+        local col=0
+        
+        curr_idx=$(( $curr_col + $curr_row * $colmax ))
+        
+        for option in "${options[@]}"; do
+
+            row=$(( $idx/$colmax ))
+            col=$(( $idx - $row * $colmax ))
+
+            cursor_to $(( $startrow + $row + 1)) $(( $offset * $col + 1))
+            if [ $idx -eq $curr_idx ]; then
+                print_selected "$option"
+            else
+                print_option "$option"
+            fi
+            ((idx++))
+        done
+    }
+
+    # initially print empty new lines (scroll down if at bottom of screen)
+    for opt; do printf "\n"; done
+
+    # determine current screen position for overwriting the options
+    local return_value=$1
+    local lastrow=`get_cursor_row`
+    local lastcol=`get_cursor_col`
+    local startrow=$(($lastrow - $#))
+    local startcol=1
+    local lines=$( tput lines )
+    local cols=$( tput cols ) 
+    local colmax=$2
+    local offset=$(( $cols / $colmax ))
+
+    local size=$4
+    #shift 4
+
+    # ensure cursor and input echoing back on upon a ctrl+c during read -s
+    trap "cursor_blink_on; stty echo; printf '\n'; exit" 2
+    cursor_blink_off
+
+    local active_row=0
+    local active_col=0
+    while true; do
+        print_options_multicol $active_col $active_row 
+        # user key control
+        case `key_input` in
+            enter)  break;;
+            up)     ((active_row--));
+                    if [ $active_row -lt 0 ]; then active_row=0; fi;;
+            down)   ((active_row++));
+                    if [ $active_row -ge $(( ${#options[@]} / $colmax ))  ]; then active_row=$(( ${#options[@]} / $colmax )); fi;;
+            left)     ((active_col=$active_col - 1));
+                    if [ $active_col -lt 0 ]; then active_col=0; fi;;
+            right)     ((active_col=$active_col + 1));
+                    if [ $active_col -ge $colmax ]; then active_col=$(( $colmax - 1 )) ; fi;;
+        esac
+    done
+
+    # cursor position back to normal
+    cursor_to $lastrow
+    printf "\n"
+    cursor_blink_on
+
+    return $(( $active_col + $active_row * $colmax ))
+}
+
+# @description This function will handle file systems. At this movement we are handling only
+# btrfs and ext4. Others will be added in future.
+filesystem () {
+echo -ne "
+Please Select your file system for both boot and root
+"
+options=("ext4" "exit")
+select_option $? 1 "${options[@]}"
+
+case $? in
+0) set_option FS ext4;;
+1) exit ;;
+*) echo "Wrong option please select again"; filesystem;;
+esac
+}
+
+# @description Choose whether drive is SSD or not.
+drivessd () {
+echo -ne "
+Is this an ssd? yes/no:
+"
+
+options=("Yes" "No")
+select_option $? 1 "${options[@]}"
+
+case ${options[$?]} in
+    y|Y|yes|Yes|YES)
+    set_option MOUNT_OPTIONS "noatime,compress=zstd,ssd,commit=120";;
+    n|N|no|NO|No)
+    set_option MOUNT_OPTIONS "noatime,compress=zstd,commit=120";;
+    *) echo "Wrong option. Try again";drivessd;;
+esac
+}
+
+# @description Disk selection for drive to be used with installation.
+diskpart () {
+echo -ne "
+------------------------------------------------------------------------
+    THIS WILL FORMAT AND DELETE ALL DATA ON THE DISK
+    Please make sure you know what you are doing because
+    after formating your disk there is no way to get data back
+------------------------------------------------------------------------
+
+"
+
+PS3='
+Select the disk to install on: '
+options=($(lsblk -n --output TYPE,KNAME,SIZE | awk '$1=="disk"{print "/dev/"$2"|"$3}'))
+
+select_option $? 1 "${options[@]}"
+disk=${options[$?]%|*}
+
+echo -e "\n${disk%|*} selected \n"
+    set_option DISK ${disk%|*}
+
+drivessd
+}
+
+# Starting functions
+background_checks
+clear
+diskpart
+clear
+filesystem
+clear
+
+echo -ne "
+
+-------------------------------------------------------------------------
+                    Automated Arch Linux Installer
+-------------------------------------------------------------------------
+
+Setting up mirrors for optimal download
+"
+source $CONFIGS_DIR/setup.conf
+iso=$(curl -4 ifconfig.co/country-iso)
+timedatectl set-ntp true
+pacman -S --noconfirm archlinux-keyring #update keyrings to latest to prevent packages failing to install
+pacman -S --noconfirm --needed pacman-contrib terminus-font
+setfont ter-v22b
+sed -i 's/^#ParallelDownloads = 5/ParallelDownloads = 10/' /etc/pacman.conf
+pacman -S --noconfirm --needed reflector rsync grub
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+echo -ne "
+-------------------------------------------------------------------------
+                    Setting up $iso mirrors for faster downloads
+-------------------------------------------------------------------------
+"
+reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
+mkdir /mnt &>/dev/null # Hiding error message if any
+echo -ne "
+-------------------------------------------------------------------------
+                    Installing Prerequisites
+-------------------------------------------------------------------------
+"
+pacman -S --noconfirm --needed gptfdisk btrfs-progs glibc
+echo -ne "
+-------------------------------------------------------------------------
+                    Formating Disk
+-------------------------------------------------------------------------
+"
+umount -A --recursive /mnt # make sure everything is unmounted before we start
+# disk prep
+sgdisk -Z ${DISK} # zap all on disk
+sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
+
+# create partitions
+sgdisk -n 1::+1M --typecode=1:ef02 --change-name=1:'BIOSBOOT' ${DISK} # partition 1 (BIOS Boot Partition)
+sgdisk -n 2::+300M --typecode=2:ef00 --change-name=2:'EFIBOOT' ${DISK} # partition 2 (UEFI Boot Partition)
+sgdisk -n 3::-0 --typecode=3:8300 --change-name=3:'ROOT' ${DISK} # partition 3 (Root), default start, remaining
+if [[ ! -d "/sys/firmware/efi" ]]; then # Checking for bios system
+    sgdisk -A 1:set:2 ${DISK}
+fi
+partprobe ${DISK} # reread partition table to ensure it is correct
+
+# make filesystems
+echo -ne "
+-------------------------------------------------------------------------
+                    Creating Filesystems
+-------------------------------------------------------------------------
+"
+if [[ "${DISK}" =~ "nvme" ]]; then
+    partition2=${DISK}p2
+    partition3=${DISK}p3
+else
+    partition2=${DISK}2
+    partition3=${DISK}3
+fi
+
+if [[ "${FS}" == "ext4" ]]; then
+    mkfs.vfat -F32 -n "EFIBOOT" ${partition2}
+    mkfs.ext4 -L ROOT ${partition3}
+    mount -t ext4 ${partition3} /mnt
+fi
+
+# mount target
+mkdir -p /mnt/boot/efi
+mount -t vfat -L EFIBOOT /mnt/boot/
+
+if ! grep -qs '/mnt' /proc/mounts; then
+    echo "Drive is not mounted can not continue"
+    echo "Rebooting in 3 Seconds ..." && sleep 1
+    echo "Rebooting in 2 Seconds ..." && sleep 1
+    echo "Rebooting in 1 Second ..." && sleep 1
+    reboot now
+fi
+echo -ne "
+-------------------------------------------------------------------------
+                    Arch Install on Main Drive
+-------------------------------------------------------------------------
+"
+pacstrap /mnt base base-devel linux linux-firmware vim sudo archlinux-keyring wget libnewt --noconfirm --needed
+echo "keyserver hkp://keyserver.ubuntu.com" >> /mnt/etc/pacman.d/gnupg/gpg.conf
+cp -R $dir /mnt/root/.src/arch-default
+cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
+
+genfstab -L /mnt >> /mnt/etc/fstab
+echo " 
+  Generated /etc/fstab:
+"
+cat /mnt/etc/fstab
+
+echo -ne "
+-------------------------------------------------------------------------
+                   Arch-Chroot
+-------------------------------------------------------------------------
+"
+arch-chroot /mnt
---
+++ 1.two.sh
@@ -0,0 +1,373 @@
+#!/bin/sh
+
+dir=$(pwd) 
+CONFIGS_DIR=$dir/configs/
+CONFIG_FILE=$dir/configs/setup.conf
+if [ ! -f $CONFIG_FILE ]; then # check if file exists
+    touch -f $CONFIG_FILE # create file if not exists
+fi
+
+# Nome do arquivo de log
+log_file="1.log"
+
+# Redireciona a saída padrão e a saída de erro padrão para o arquivo de log
+exec > >(tee -a "$log_file") 2>&1
+
+set_option() {
+    if grep -Eq "^${1}.*" $CONFIG_FILE; then # check if option exists
+        sed -i -e "/^${1}.*/d" $CONFIG_FILE # delete option if exists
+    fi
+    echo "${1}=${2}" >>$CONFIG_FILE # add option
+}
+
+select_option() {
+
+    # little helpers for terminal print control and key input
+    ESC=$( printf "\033")
+    cursor_blink_on()  { printf "$ESC[?25h"; }
+    cursor_blink_off() { printf "$ESC[?25l"; }
+    cursor_to()        { printf "$ESC[$1;${2:-1}H"; }
+    print_option()     { printf "$2   $1 "; }
+    print_selected()   { printf "$2  $ESC[7m $1 $ESC[27m"; }
+    get_cursor_row()   { IFS=';' read -sdR -p $'\E[6n' ROW COL; echo ${ROW#*[}; }
+    get_cursor_col()   { IFS=';' read -sdR -p $'\E[6n' ROW COL; echo ${COL#*[}; }
+    key_input()         {
+                        local key
+                        IFS= read -rsn1 key 2>/dev/null >&2
+                        if [[ $key = ""      ]]; then echo enter; fi;
+                        if [[ $key = $'\x20' ]]; then echo space; fi;
+                        if [[ $key = "k" ]]; then echo up; fi;
+                        if [[ $key = "j" ]]; then echo down; fi;
+                        if [[ $key = "h" ]]; then echo left; fi;
+                        if [[ $key = "l" ]]; then echo right; fi;
+                        if [[ $key = "a" ]]; then echo all; fi;
+                        if [[ $key = "n" ]]; then echo none; fi;
+                        if [[ $key = $'\x1b' ]]; then
+                            read -rsn2 key
+                            if [[ $key = [A || $key = k ]]; then echo up;    fi;
+                            if [[ $key = [B || $key = j ]]; then echo down;  fi;
+                            if [[ $key = [C || $key = l ]]; then echo right;  fi;
+                            if [[ $key = [D || $key = h ]]; then echo left;  fi;
+                        fi 
+    }
+    print_options_multicol() {
+        # print options by overwriting the last lines
+        local curr_col=$1
+        local curr_row=$2
+        local curr_idx=0
+
+        local idx=0
+        local row=0
+        local col=0
+        
+        curr_idx=$(( $curr_col + $curr_row * $colmax ))
+        
+        for option in "${options[@]}"; do
+
+            row=$(( $idx/$colmax ))
+            col=$(( $idx - $row * $colmax ))
+
+            cursor_to $(( $startrow + $row + 1)) $(( $offset * $col + 1))
+            if [ $idx -eq $curr_idx ]; then
+                print_selected "$option"
+            else
+                print_option "$option"
+            fi
+            ((idx++))
+        done
+    }
+
+    # initially print empty new lines (scroll down if at bottom of screen)
+    for opt; do printf "\n"; done
+
+    # determine current screen position for overwriting the options
+    local return_value=$1
+    local lastrow=`get_cursor_row`
+    local lastcol=`get_cursor_col`
+    local startrow=$(($lastrow - $#))
+    local startcol=1
+    local lines=$( tput lines )
+    local cols=$( tput cols ) 
+    local colmax=$2
+    local offset=$(( $cols / $colmax ))
+
+    local size=$4
+    #shift 4
+
+    # ensure cursor and input echoing back on upon a ctrl+c during read -s
+    trap "cursor_blink_on; stty echo; printf '\n'; exit" 2
+    cursor_blink_off
+
+    local active_row=0
+    local active_col=0
+    while true; do
+        print_options_multicol $active_col $active_row 
+        # user key control
+        case `key_input` in
+            enter)  break;;
+            up)     ((active_row--));
+                    if [ $active_row -lt 0 ]; then active_row=0; fi;;
+            down)   ((active_row++));
+                    if [ $active_row -ge $(( ${#options[@]} / $colmax ))  ]; then active_row=$(( ${#options[@]} / $colmax )); fi;;
+            left)     ((active_col=$active_col - 1));
+                    if [ $active_col -lt 0 ]; then active_col=0; fi;;
+            right)     ((active_col=$active_col + 1));
+                    if [ $active_col -ge $colmax ]; then active_col=$(( $colmax - 1 )) ; fi;;
+        esac
+    done
+
+    # cursor position back to normal
+    cursor_to $lastrow
+    printf "\n"
+    cursor_blink_on
+
+    return $(( $active_col + $active_row * $colmax ))
+}
+
+echo -ne "
+
+-------------------------------------------------------------------------
+                    Setting: timezone | locale | keymap | hostname
+-------------------------------------------------------------------------
+
+Setfont 22
+"
+pacman -S --noconfirm --needed pacman-contrib terminus-font
+setfont ter-v22b
+sed -i 's/^#ParallelDownloads = 5/ParallelDownloads = 10/' /etc/pacman.conf
+
+echo -ne "Setting Timezone"
+
+# Installation guide, Installation, Configure the system, Time zone
+ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+
+echo -ne "Setting Locale and Keymap"
+
+sed -i 's/^en_US.UTF-8 UTF-8/#en_US.UTF-8 UTF-8/' /etc/locale.gen
+sed -i 's/^#pt_BR.UTF-8 UTF-8/pt_BR.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
+timedatectl --no-ask-password set-ntp 1
+localectl --no-ask-password set-locale LANG="pt_BR.UTF-8" LC_TIME="pt_BR.UTF-8"
+# Set keymaps
+echo 'KEYMAP=br-abnt2
+XKBLAYOUT=br
+XKBMODEL=abnt2
+XKBOPTIONS=terminate:ctrl_alt_bksp' | tee /etc/vsconsole.conf >/dev/null
+localectl --no-ask-password set-x11-keymap br abnt2
+
+echo -ne "
+-------------------------------------------------------------------------
+                    Network Setup 
+-------------------------------------------------------------------------
+"
+pacman -S --noconfirm --needed networkmanager dhclient
+systemctl enable --now NetworkManager
+echo -ne "
+-------------------------------------------------------------------------
+                    Setting up mirrors for optimal download 
+-------------------------------------------------------------------------
+"
+pacman -S --noconfirm --needed pacman-contrib curl
+pacman -S --noconfirm --needed reflector rsync arch-install-scripts git
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+
+# nc=$(grep -c ^processor /proc/cpuinfo)
+# echo -ne "
+# -------------------------------------------------------------------------
+#                     You have " $nc" cores. And
+# 			changing the makeflags for "$nc" cores. Aswell as
+# 				changing the compression settings.
+# -------------------------------------------------------------------------
+# "
+# TOTAL_MEM=$(cat /proc/meminfo | grep -i 'memtotal' | grep -o '[[:digit:]]*')
+# if [[  $TOTAL_MEM -gt 8000000 ]]; then
+# sed -i "s/#MAKEFLAGS=\"-j2\"/MAKEFLAGS=\"-j$nc\"/g" /etc/makepkg.conf
+# sed -i "s/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T $nc -z -)/g" /etc/makepkg.conf
+# fi
+
+# Add sudo no password rights
+sed -i 's/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
+sed -i 's/^# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
+
+echo -ne "
+-------------------------------------------------------------------------
+                    Installing Base System  
+-------------------------------------------------------------------------
+"
+sudo pacman -S --noconfirm --needed - < $dir/pacman-pkgs.txt
+
+echo -ne "
+-------------------------------------------------------------------------
+                    Installing Microcode
+-------------------------------------------------------------------------
+"
+# determine processor type and install microcode
+proc_type=$(lscpu)
+if grep -E "GenuineIntel" <<< ${proc_type}; then
+    echo "Installing Intel microcode"
+    pacman -S --noconfirm --needed intel-ucode
+    proc_ucode=intel-ucode.img
+elif grep -E "AuthenticAMD" <<< ${proc_type}; then
+    echo "Installing AMD microcode"
+    pacman -S --noconfirm --needed amd-ucode
+    proc_ucode=amd-ucode.img
+fi
+
+echo -ne "
+-------------------------------------------------------------------------
+                    Installing Graphics Drivers
+-------------------------------------------------------------------------
+"
+# Graphics Drivers find and install
+gpu_type=$(lspci)
+if grep -E "NVIDIA|GeForce" <<< ${gpu_type}; then
+    pacman -S --noconfirm --needed nvidia
+	nvidia-xconfig
+elif lspci | grep 'VGA' | grep -E "Radeon|AMD"; then
+    pacman -S --noconfirm --needed xf86-video-amdgpu
+elif grep -E "Integrated Graphics Controller" <<< ${gpu_type}; then
+    pacman -S --noconfirm --needed libva-intel-driver libvdpau-va-gl vulkan-intel libva-intel-driver libva-utils
+elif grep -E "Intel Corporation UHD" <<< ${gpu_type}; then
+    pacman -S --needed --noconfirm libva-intel-driver libvdpau-va-gl vulkan-intel libva-intel-driver libva-utils
+elif grep -E "VMware SVGA II Adapter" <<< ${gpu_type}; then
+    pacman -S --needed --noconfirm open-vm-tools
+    systemctl enable vmtoolsd.service
+fi
+
+# @description Gather username and password to be used for installation. 
+userinfo () {
+read -p "Please enter your username: " username
+set_option USERNAME ${username,,} # convert to lower case as in issue #109 
+set_password "PASSWORD"
+read -rep "Please enter your hostname: " nameofmachine
+set_option NAME_OF_MACHINE $nameofmachine
+}
+
+logo () {
+# This will be shown on every set as user is progressing
+echo -ne "
+------------------------------------------------------------------------
+            Please select presetup settings for your system              
+------------------------------------------------------------------------
+"
+}
+
+# list of functions
+logo
+userinfo
+clear
+
+#SETUP IS WRONG THIS IS RUN
+if ! source $dir/configs/setup.conf; then
+	# Loop through user input until the user gives a valid username
+	while true
+	do 
+		read -p "Please enter username:" username
+		# username regex per response here https://unix.stackexchange.com/questions/157426/what-is-the-regex-to-validate-linux-users
+		# lowercase the username to test regex
+		if [[ "${username,,}" =~ ^[a-z_]([a-z0-9_-]{0,31}|[a-z0-9_-]{0,30}\$)$ ]]
+		then 
+			break
+		fi 
+		echo "Incorrect username."
+	done 
+# convert name to lowercase before saving to setup.conf
+echo "username=${username,,}" >> $dir/configs/setup.conf
+
+    #Set Password
+    read -p "Please enter password:" password
+echo "password=${password,,}" >> $dir/configs/setup.conf
+
+    # Loop through user input until the user gives a valid hostname, but allow the user to force save 
+	while true
+	do 
+		read -p "Please name your machine:" name_of_machine
+		# hostname regex (!!couldn't find spec for computer name!!)
+		if [[ "${name_of_machine,,}" =~ ^[a-z][a-z0-9_.-]{0,62}[a-z0-9]$ ]]
+		then 
+			break 
+		fi 
+		# if validation fails allow the user to force saving of the hostname
+		read -p "Hostname doesn't seem correct. Do you still want to save it? (y/n)" force 
+		if [[ "${force,,}" = "y" ]]
+		then 
+			break 
+		fi 
+	done 
+
+    echo "NAME_OF_MACHINE=${name_of_machine,,}" >> $dir/configs/setup.conf
+fi
+
+echo -ne "
+-------------------------------------------------------------------------
+                    Adding User
+-------------------------------------------------------------------------
+"
+if [ $(whoami) = "root"  ]; then    
+    useradd -m -G wheel -s /bin/bash $USERNAME 
+    echo "$USERNAME created, home directory created, added to wheel group, default shell set to /bin/bash"
+
+# use chpasswd to enter $USERNAME:$password
+    echo "$USERNAME:$PASSWORD" | chpasswd
+    echo "$USERNAME password set"
+
+	# cp -R $HOME/ArchTitus /home/$USERNAME/
+    # chown -R $USERNAME: /home/$USERNAME/ArchTitus
+    # echo "ArchTitus copied to home directory"
+
+# enter $NAME_OF_MACHINE to /etc/hostname
+	echo $NAME_OF_MACHINE > /etc/hostname
+else
+	echo "You are already a user proceed with aur installs"
+fi
+
+echo -ne "
+-------------------------------------------------------------------------
+                    Enabling Essential Services
+-------------------------------------------------------------------------
+"
+# ntpd -qg
+# systemctl enable ntpd.service
+# echo "  NTP enabled"
+systemctl disable dhcpcd.service
+echo "  DHCP disabled"
+systemctl stop dhcpcd.service
+echo "  DHCP stopped"
+systemctl enable NetworkManager.service
+echo "  NetworkManager enabled"
+
+echo -ne "
+-------------------------------------------------------------------------
+                    Cleaning
+-------------------------------------------------------------------------
+"
+# Remove no password sudo rights
+sed -i 's/^%wheel ALL=(ALL) NOPASSWD: ALL/# %wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
+sed -i 's/^%wheel ALL=(ALL:ALL) NOPASSWD: ALL/# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
+# Add sudo rights
+sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
+sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+
+echo "Grub"
+pacman -S grub efibootmgr --noconfirm --needed
+
+
+if [[ -d "/sys/firmware/efi" ]]; then
+    grub-install --target=i386-pc --recheck /dev/sda1
+elseif
+    grub-install --target=x86_64-efi --efi-directory=/boot/efi
+fi
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "check log for error"
+
+echo "then"
+# Installation guide, Installation, Reboot
+echo "
+exit
+umount -R /mnt
+reboot
+"
+
+echo "install default ?"
